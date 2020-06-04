1ac04b4cb7a345a26a3712b96117d4a452b3110b
suppenkasperl <wi17b015@technikum-wien.at>
2020-06-04 19:26:06 +0200

    add navigation, add fragments

--- app/src/main/java/com/spoelt/moviefavourites/MainActivity.kt
+++ app/src/main/java/com/spoelt/moviefavourites/MainActivity.kt
@@ -2,11 +2,21 @@ package com.spoelt.moviefavourites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onSupportNavigateUp() = Navigation.findNavController(
+        this,
+        R.id.nav_host_fragment
+    ).navigateUp()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nav_host_fragment.findNavController()
     }
 }
